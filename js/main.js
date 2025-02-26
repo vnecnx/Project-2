600993712f655fadbd2dc6baa52f0aa2b08483c1
vnecnx <ernestkfowler@gmail.com>
2025-02-26 12:23:13 -0600

    Added content and did a lot of CSS styling

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 // JS scripts placed here
+
+// Note to Quinton: I'd normally delete this but I'd like to keep it here hidden in case I wanna reference it later down the line
+// const logotxt = document.querySelector('.logo-txt');
+// console.log('.logo-txt', logotxt)
+// logotxt.textContent = 'Hello world'
+// logotxt.innerHTML = '<p>New HTML Content</p>'
+// logotxt.setAttribute('class','logo-txt-new')
+
+const alertbutton = document.querySelector('.alertbutton')
+alertbutton.addEventListener('click', () => {
+    alert("Thank you!")
+})
